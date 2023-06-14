539ba2f1f1f7347de34192c7582088da384db96a
clevovi <130769749+clevovi@users.noreply.github.com>
2023-06-15 00:48:10 +0300

    Add files via upload

---
+++ load_dimension.py
@@ -0,0 +1,34 @@
+from airflow.hooks.postgres_hook import PostgresHook
+from airflow.models import BaseOperator
+from airflow.utils.decorators import apply_defaults
+
+class LoadDimensionOperator(BaseOperator):
+
+    ui_color = '#80BD9E'
+
+    @apply_defaults
+    def __init__(self,
+                 redshift_conn_id="", 
+                 sql_query="", 
+                 table="",
+                 load_mode="truncate_insert",
+                 # Define your operators params (with defaults) here
+                 # Example:
+                 # conn_id = your-connection-name
+                 *args, **kwargs):
+
+        super(LoadDimensionOperator, self).__init__(*args, **kwargs)
+        self.redshift_conn_id = redshift_conn_id
+        self.table = table
+        self.sql_query = sql_query
+        self.load_mode = load_mode
+        # Map params here
+        # Example:
+        # self.conn_id = conn_id
+
+    def execute(self, context):
+        redshift_hook = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+        if self.load_mode == "truncate_insert":
+           self.log.info("Clearing data from destination Redshift table")
+           redshift_hook.run(f"TRUNCATE TABLE {self.table}")
+        redshift_hook.run(self.sql_query)
